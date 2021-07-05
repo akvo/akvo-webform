54b730dd415e332bd7833d2096725782d30cda0b
dedenbangkit <mail@dedenbangkit.com>
2021-07-06 00:10:06 +0700

    [#1] Prettier before make changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './Home'
-import Error from './Error'
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { questionReducers } from './reducers/questionReducers.js'
-import { BASE_URL, CACHE_URL } from './util/Environment.js'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Error from "./Error";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { questionReducers } from "./reducers/questionReducers.js";
+import { BASE_URL, CACHE_URL } from "./util/Environment.js";
 
-const store = createStore(questionReducers)
+const store = createStore(questionReducers);
 
 class App extends Component {
-
-    render () {
-        let path = "/:instance/:surveyid"
-        if (CACHE_URL) {
-            path = path + "/:cacheid";
-        }
-        return (
-	        <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path={ BASE_URL + "/"} render={ props => <Error/>} />
-                    <Route exact path={ BASE_URL + "/:instance"} render={props => <Error/>} />
-                    <Route exact path={ BASE_URL + path} render={ props => <Home key="home" {...props} />} />
-                </Switch>
-            </BrowserRouter>
-	        </Provider>
-        )
+  render() {
+    let path = "/:instance/:surveyid";
+    if (CACHE_URL) {
+      path = path + "/:cacheid";
     }
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={BASE_URL + "/"} render={(props) => <Error />} />
+            <Route
+              exact
+              path={BASE_URL + "/:instance"}
+              render={(props) => <Error />}
+            />
+            <Route
+              exact
+              path={BASE_URL + path}
+              render={(props) => <Home key="home" {...props} />}
+            />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- frontend/src/util/Environment.js
+++ frontend/src/util/Environment.js
@@ -1,10 +1,15 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const API_URL = process.env.NODE_ENV !== "development"
+export const API_URL =
+  process.env.NODE_ENV !== "development"
     ? window.location.origin + "/" + process.env.REACT_APP_API_URL + "/"
-    : process.env.REACT_APP_API_URL + "/"
-export const CACHE_URL = window.location.href.split('/').splice(-1)[0].split('-').length === 1 ? false : true;
+    : process.env.REACT_APP_API_URL + "/";
+export const CACHE_URL =
+  window.location.href.split("/").splice(-1)[0].split("-").length === 1
+    ? false
+    : true;
 export const PARENT_URL = window.location !== window.parent.location;
 export const USING_PASSWORDS = "2scale";
-export const SAVE_FEATURES = ["2scale","seap"];
+export const SAVE_FEATURES = ["2scale", "seap"];
 export const CAPTCHA_KEY = process.env.REACT_APP_CAPTCHA_KEY;
-export const READ_CACHE = process.env.NODE_ENV === "development" ? "fetch" : "update";
+export const READ_CACHE =
+  process.env.NODE_ENV === "development" ? "fetch" : "update";
