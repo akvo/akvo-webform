a9eba59ea4d273718b8c0d60ec87b6531df0557c
dedenbangkit <mail@dedenbangkit.com>
2021-07-26 18:57:09 +0700

    [#2] Fix shellcheck

--- get-services.sh
+++ get-services.sh
@@ -1,17 +1,21 @@
-git clone git@github.com:akvo/akvo-flow-server-config.git
+#!/usr/bin/env bash
+
+serverconfig="akvo/akvo-flow-server-config.git"
+curl -H "Authorization: token ${TOKEN}" \
+    -L https://api.github.com/repos/${serverconfig}/tarball > akvo-flow-server-config.tar.gz
+
 echo 'bucket,service,instances' > ./data/flow-survey-amazon-aws.csv
 list=$(find ./akvo-flow-server-config -type f -name appengine-web.xml \
     -maxdepth 2 -mindepth 2 \
     -exec echo {} \; 2>/dev/null)
 for word in ${list};
 do
-    service=$(cat ${word} \
-        | grep "<application>" \
+    service=$(grep "<application>" "${word}"\
         | sed 's/<.*>\(.*\)<.*>/\1/' \
         | sed 's/\ //g')
-    bucket=$(cat ${word} | grep s3bucket \
+    bucket=$(grep "s3bucket" "${word}"\
         | sed 's/.*value="\([^"]*\).*/\1/')
-    instance=$(cat ${word} | grep alias \
+    instance=$(grep "alias" "${word}"\
         | sed 's/.*value="\([^"]*\).*/\1/' \
         | cut -d '.' -f 1)
     echo "${bucket},${service},${instance}" | sed 's/\ //g' >> ./data/flow-survey-amazon-aws.csv;
