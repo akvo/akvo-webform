ac818de9b90d05ace1526c237a65597019d12baf
dedenbangkit <mail@dedenbangkit.com>
2021-07-26 12:57:52 +0700

    [#2] Modify script to use xml instead

--- get-services.sh
+++ get-services.sh
@@ -1,16 +1,20 @@
 git clone git@github.com:akvo/akvo-flow-server-config.git
-echo 'bucket,service,instances' > ./backend/data/flow-survey-amazon-aws.csv
-list=$(find ./akvo-flow-server-config -type f -name survey.properties \
+echo 'bucket,service,instances' > ./data/flow-survey-amazon-aws.csv
+list=$(find ./akvo-flow-server-config -type f -name appengine-web.xml \
     -maxdepth 2 -mindepth 2 \
     -exec echo {} \; 2>/dev/null)
 for word in ${list};
 do
-    service=$(echo ${word} | cut -d '/' -f 3)
-    bucket=$(cat ${word} | grep awsBucket | cut -d '=' -f 2)
-    instance=$(cat ${word} | grep instanceUrl | cut -d '=' -f 2 \
-        | sed 's/https\:\/\///g' \
-        | sed 's/.akvoflow.org//g' \
-        | sed 's/.appspot.com//g')
-    echo "${bucket},${service},${instance}" | sed 's/\ //g' >> ./backend/data/flow-survey-amazon-aws.csv;
+    service=$(cat ${word} \
+        | grep "<application>" \
+        | sed 's/<.*>\(.*\)<.*>/\1/' \
+        | sed 's/\ //g')
+    bucket=$(cat ${word} | grep s3bucket \
+        | sed 's/.*value="\([^"]*\).*/\1/')
+    instance=$(cat ${word} | grep alias \
+        | sed 's/.*value="\([^"]*\).*/\1/' \
+        | cut -d '.' -f 1)
+    echo "${bucket},${service},${instance}" | sed 's/\ //g' >> ./data/flow-survey-amazon-aws.csv;
 done;
+
 rm -rf akvo-flow-server-config
