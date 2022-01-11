e334ebbdec380aa9472030f994b431524a147761
dedenbangkit <mail@dedenbangkit.com>
2022-01-11 17:43:39 +0700

    [#10] store context, initial store value

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,5 +1,5 @@
 export const defaultValue = {
-  form: {
+  forms: {
     altText: [],
     name: "Loading...",
     questionGroup: "Loading...",
@@ -11,17 +11,36 @@ export const defaultValue = {
         question: [
           {
             id: 0,
+            order: 1,
             altText: [],
             help: { altText: [], text: "Loading" },
             localeNameFlag: true,
             type: "free",
+            mandatory: false,
             text: "Loading...",
           },
+          {
+            id: 1,
+            order: 2,
+            altText: [],
+            help: { altText: [], text: "Loading" },
+            localeNameFlag: true,
+            type: "free",
+            mandatory: false,
+            text: "Loading...",
+            validationRule: {
+              allowDecimal: false,
+              signed: false,
+              validationType: "numeric",
+            },
+            dependency: [{ answerValue: [], question: 0 }],
+          },
         ],
+        repeatable: true,
       },
     ],
   },
-  answer: [],
+  answers: [],
   active: {
     questionGroup: null,
     repeatQuestionGroup: 1,
@@ -39,11 +58,11 @@ const removeAnswer = (answer, input) => {
 const reducer = (state, action) => {
   switch (action.type) {
     case "INIT FORM":
-      return { ...state, form: action.form };
+      return { ...state, forms: action.forms };
     case "ADD ANSWER":
-      return { ...state, answer: addAnswer(state.answer, action.input) };
+      return { ...state, answers: addAnswer(state.answer, action.input) };
     case "REMOVE ANSWER":
-      return { ...state, answer: removeAnswer(state.answer, action.input) };
+      return { ...state, answers: removeAnswer(state.answer, action.input) };
     default:
       throw new Error();
   }
