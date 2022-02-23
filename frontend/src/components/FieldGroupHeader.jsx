4c7cb3bf51cd8b9572d7863669427ef7da9dae81
wayangalihpratama <wgprtm@gmail.com>
2022-02-23 19:19:03 +0800

    [#33] Load translation on question group header

--- frontend/src/components/FieldGroupHeader.jsx
+++ frontend/src/components/FieldGroupHeader.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Row, Col, Input, Button } from "antd";
 import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
 import { MdRepeat } from "react-icons/md";
 import dataProviders from "../store";
 import { updateRepeat } from "../lib/form";
 
-const FieldGroupHeader = ({ index, heading, repeatable }) => {
+const FieldGroupHeader = ({ index, heading, repeatable, altText }) => {
   const state = dataProviders.Values();
   const dispatch = dataProviders.Actions();
-  const { forms } = state;
+  const { forms, language } = state;
   const current = forms.questionGroup.find((x) => x.index === index);
+  const activeLang = language?.active;
+
+  const langText = useMemo(() => {
+    const findLang = altText?.find((x) => x?.language === activeLang);
+    return findLang?.text ? (
+      <>
+        {" "}
+        &#47; <span className="translation-text">{findLang.text}</span>
+      </>
+    ) : (
+      ""
+    );
+  }, [altText, activeLang]);
 
   if (!repeatable) {
-    return <div className="field-group-header">{heading}</div>;
+    return (
+      <div className="field-group-header">
+        {heading}
+        {langText}
+      </div>
+    );
   }
 
   const { repeat } = current;
 
   return (
     <div className="field-group-header">
-      <div className="field-group-heading">{heading}</div>
+      <div className="field-group-heading">
+        {heading}
+        {langText}
+      </div>
       {repeatable && (
         <div className="field-group-heading">
           <MdRepeat className="icon" />
