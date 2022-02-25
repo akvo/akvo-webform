fe34abcaff81bcebadc8fa48838a5612c74b6bc3
wayangalihpratama <wgprtm@gmail.com>
2022-02-25 16:43:06 +0800

    [#39] Style improvement

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import { Row, Col, Form, Input, Button, Typography } from "antd";
+import { Row, Col, Form, Input, Button, Typography, Space } from "antd";
 import dataProviders from "../store";
 
 const { Text, Link } = Typography;
@@ -37,68 +37,82 @@ const Login = () => {
   }, [isAuthError]);
 
   return (
-    <Row className="main login-container" align="middle" justify="center">
-      <Col xs={20} sm={16} md={12} lg={10} xl={8} xxl={6}>
-        <div className="login-header">
-          <h1 className="logo">A.</h1>
-          <h1>AkvoFlow Forms</h1>
-          <h2>{surveyTitle}</h2>
-        </div>
-        <div className="login-form">
-          <div className="login-form-title">
-            <h4>Log in to submit data</h4>
-            {authError}
+    <div className="main login-container">
+      <Row align="middle" justify="center" className="login-header-wrapper">
+        <Col xs={20} sm={16} md={12} lg={10} xl={8} xxl={6}>
+          <div className="login-header">
+            <h1 className="logo">
+              <img src="/favicon-96x96.png" alt="akvoflow-webforms-logo" />
+            </h1>
+            <h1>AkvoFlow WebForms</h1>
           </div>
-          <Form
-            name="login"
-            layout="vertical"
-            labelCol={{ span: 24 }}
-            wrapperCol={{ span: 24 }}
-            onFinish={onFinish}
-          >
-            <Form.Item
-              label="Submitter"
-              name="submitter"
-              rules={[{ required: true, message: "Submitter name required." }]}
+        </Col>
+      </Row>
+      <Row align="middle" justify="center">
+        <Col xs={20} sm={16} md={12} lg={10} xl={8} xxl={6}>
+          <div className="survey-title-wrapper">
+            <h2>{surveyTitle}</h2>
+          </div>
+          <div className="login-form">
+            <div className="login-form-title">
+              <h4>Log in to submit data</h4>
+              {authError}
+            </div>
+            <Form
+              name="login"
+              layout="vertical"
+              labelCol={{ span: 24 }}
+              wrapperCol={{ span: 24 }}
+              onFinish={onFinish}
             >
-              <Input size="large" />
-            </Form.Item>
-            <Form.Item
-              label={
-                <span>
-                  Password{" "}
-                  <Text italic>
-                    * please contact administrator for the password.
-                  </Text>
-                </span>
-              }
-              name="password"
-              rules={[{ required: true, message: "Password required." }]}
-            >
-              <Input.Password size="large" />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="button-next"
-                size="large"
-                block
+              <Form.Item
+                label="Submitter name"
+                name="submitter"
+                rules={[
+                  { required: true, message: "Submitter name required." },
+                ]}
               >
-                Start
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-        <div className="login-footer">
-          <Text>AkvoFlow Webforms enabling remote Byod data collection. </Text>
-          <Link>Data policy</Link>.{" "}
-          <Link href="https://akvo.org/" target="_blank">
-            Akvo.org
-          </Link>
-        </div>
-      </Col>
-    </Row>
+                <Input size="large" />
+              </Form.Item>
+              <Form.Item
+                label={
+                  <Space direction="vertical" size={0}>
+                    Form Password
+                    <Text italic>
+                      * please contact administrator for the password.
+                    </Text>
+                  </Space>
+                }
+                name="password"
+                rules={[{ required: true, message: "Password required." }]}
+              >
+                <Input.Password size="large" />
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="button-next"
+                  size="large"
+                  block
+                >
+                  Start
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+          <div className="login-footer">
+            <Text>
+              AkvoFlow Webforms enabling remote Byod data collection.{" "}
+            </Text>
+            <Link>Data policy</Link>.{" "}
+            <Link href="https://akvo.org/" target="_blank">
+              Akvo.org
+            </Link>
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
